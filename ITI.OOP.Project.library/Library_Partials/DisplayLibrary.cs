bb80e095a0ec4c528dcda6040b2f9080bba0151c
Ahmed <ahmedsaberkh7@gmail.com>
2025-08-10 12:52:08 +0300

    add display library

--- ITI.OOP.Project.library/Library_Partials/DisplayLibrary.cs
+++ ITI.OOP.Project.library/Library_Partials/DisplayLibrary.cs
@@ -4,9 +4,19 @@
     {
         public void ListBooks()
         {
+            foreach (var book in Books)
+            {
+
+                Console.WriteLine($"ID: {book.ID}, Title: {book.Title}, Author: {book.Author}, Available: {book.Availability}");
+            }
         }
         public void ListMembers()
         {
+            foreach (var member in Members)
+            {
+                Console.WriteLine($"ID: {member.ID}, Name: {member.Name}, Borrowed Books:{member.borrowedBooks}");
+            }
+
         }
     }
 }
