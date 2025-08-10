5ab938203001a4f16915c9a68e234e230a401dbc
Youssef Arram <arramyoussef@gmail.com>
2025-08-10 10:47:24 +0300

    AddMemberFunction

--- ITI.OOP.Project.library/Library_Partials/MembersLibrary.cs
+++ ITI.OOP.Project.library/Library_Partials/MembersLibrary.cs
@@ -1,12 +1,67 @@
-﻿namespace oopProject.Library_Partials
+﻿
+using oopProject.Components;
+
+namespace oopProject.Library_Partials
 {
     public partial class Library
     {
         public void AddMember()
         {
-        }
+            int x;
+            do 
+            {
+               
+                 Console.Write("Enter Member ID: ");
+
+			} while (!int.TryParse(Console.ReadLine(), out x) );
+            string N;
+            do
+            {
+                Console.Write("Enter Name: ");
+
+                N = Console.ReadLine();
+
+            }
+            while (N == null || N.Length ==0);
+
+			Member member = new Member();
+			member.ID = x;
+			member.Name = N;
+			Members.Add(member);
+
+			Console.WriteLine($"Member {N} added successfully");
+
+
+		}
         public void RemoveMember()
         {
-        }
+			int x;
+			do
+			{
+
+				Console.Write("Enter Member ID: ");
+
+
+			} while (!int.TryParse(Console.ReadLine(), out x));
+
+            Member member = null; ;
+            for (int i = 0; i < Members.Count; i++)
+            {
+                if (Members[i].ID == x)
+                {
+                    member = Members[i];
+                    break;
+                }
+            }
+            if (member != null)
+            {
+                Members.Remove(member);
+            }
+            else
+            {
+                Console.WriteLine("ID is not exist");
+            }
+
+		}
     }
 }
