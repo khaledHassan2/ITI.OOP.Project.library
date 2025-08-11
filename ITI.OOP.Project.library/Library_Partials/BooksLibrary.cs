a1b8897b189eb61bb6edc5fb4899f58c510a3281
abrar <abrabad02mail.com>
2025-08-11 14:36:59 +0300

    update add method

--- ITI.OOP.Project.library/Library_Partials/BooksLibrary.cs
+++ ITI.OOP.Project.library/Library_Partials/BooksLibrary.cs
@@ -17,11 +17,25 @@ namespace oopProject.Library_Partials
         public void AddBook()
         {
             int id;
+            bool flag = false;
             do
             {
                 Console.Write("Enter Book ID: ");
-            }
-            while (!int.TryParse(Console.ReadLine(), out id));
+                flag = int.TryParse(Console.ReadLine(), out id);
+                if (flag)
+                {
+                    foreach(Book b in Books)
+                    {
+                        if (b.ID == id)
+                        {
+                            Console.WriteLine("Book ID already exists. Please enter a unique ID.");
+                            flag = false;
+                            break;
+                        }
+                    }
+                }
+            } while (!flag);
+
 
             string title;
             do
